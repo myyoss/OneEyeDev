5afb7af0d60fae58295e83cbfc85cf859a642421
Yossi <asnafy.junior@gmail.com>
2023-01-08 11:33:37 +0200

    works two

---
+++ dist/index.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var faders = document.querySelectorAll(".fade-in");
+var sliders = document.querySelectorAll(".slide-in");
+var appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+var appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+faders.forEach(function (fader) {
+  appearOnScroll.observe(fader);
+});
+sliders.forEach(function (slider) {
+  appearOnScroll.observe(slider);
+});
